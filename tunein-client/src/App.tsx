9afb4e30a1e90796cc38d32f9df562bffb226c6f
Michał Ryniewicz <misiekxyz18@gmail.com>
2022-11-02 21:29:53 +0100

    init

---
+++ tunein-client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FetchData } from './FetchData';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FetchData />
+    </div>
+  );
+}
+
+export default App;
---
+++ tunein-client/src/FetchData.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export class FetchData extends Component {
+    
+    state = {
+        forecasts: [],
+        loading: true
+    }
+
+    async componentDidMount(): Promise<void> {
+        const url = "weatherforecast";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    render(): React.ReactNode {
+        return <div>
+            {this.state.loading ? <div>loading...</div> : <div>person...</div>}
+        </div>
+    }
+
+}
