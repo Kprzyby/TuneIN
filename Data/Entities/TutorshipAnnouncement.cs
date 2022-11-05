da463af1d70016840ed6864e981ab94ec555f6fc
Kprzyby <kprzy@vp.pl>
2022-11-05 21:04:06 +0100

    added the tutorship announcement entity and view model

---
+++ Data/Entities/TutorshipAnnouncement.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+    [Table("TutorshipAnnouncement")]
+    public class TutorshipAnnouncement
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Category { get; set; }
+        public int CreatedById { get; set; }
+
+        [ForeignKey("CreatedById")]
+        public virtual User CreatedBy { get; set; }
+    }
+}
---
+++ Data/Migrations/20221105195729_AddTutorshipAnnouncementEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTutorshipAnnouncementEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TutorshipAnnouncement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedById = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TutorshipAnnouncement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TutorshipAnnouncement_User_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutorshipAnnouncement_CreatedById",
+                table: "TutorshipAnnouncement",
+                column: "CreatedById");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TutorshipAnnouncement");
+        }
+    }
+}
