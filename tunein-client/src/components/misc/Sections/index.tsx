261b8bcc3ea6efdb1d005d28875c46dac2e78d61
miloszAlejster <milosz.alejster@gmail.com>
2022-11-09 00:48:04 +0100

    init nexta

---
+++ tunein-client/src/components/misc/Sections/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// for exporting sections
+const Sections: React.FC = () => (
+  <>
+  </>
+);
+
+export default Sections;
---
+++ tunein-client/src/components/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+
+const Homepage: NextPage = () =>{
+    return(
+        <div>
+            <h1>Welcome to TuneIN</h1>
+            <h2>web design trudny ehh</h2>
+        </div>
+    );
+}
+
+export default Homepage;
