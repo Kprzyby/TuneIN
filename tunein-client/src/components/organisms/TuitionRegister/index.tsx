8ae2b20b58849a4506d198abf9da10dcc4720a30
Milosz <milosz.alejster@gmail.com>
2023-02-15 17:19:34 +0100

    added rich text editor

--- tunein-client/src/components/organisms/TuitionRegister/index.tsx
+++ tunein-client/src/components/organisms/TuitionRegister/index.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import * as Styled from './styles';
+import * as Yup from "yup"
+import useRichText from '@components/molecules/RichText';
+import { useFormik } from 'formik';
+import RgbButton from '@components/molecules/RgbButton';
+import { convertToRaw, EditorState } from 'draft-js';
 
 const TuitionRegister: React.FC = () => {
-
+  const formik = useFormik({
+      initialValues: {
+        editorState: EditorState.createEmpty(),
+        title: ""
+      },
+      validateOnBlur: false,
+      validateOnChange: false,
+      validationSchema: Yup.object({
+        title: Yup.string()
+          .min(12, "No shorter than 12 characters")
+          .max(70, "No longer than 70 characters")
+          .required("Title is required"),
+      }),
+      onSubmit: () => {
+        // convertToRaw(editorState.getCurrentContent()) returns json
+      }
+  })
+  const {renderRichText, editorState} = useRichText();
   return (
     <Styled.Wrapper>
-    <div>TuitionRegister</div>
+      <Styled.Form onSubmit={formik.handleSubmit}>
+      {/*Title*/}
+      <Styled.TileTitle variant="PasswordTileTitle">Title</Styled.TileTitle>
+      <Styled.Input placeholder="Title" id="title"
+          value={formik.values.title} 
+          onChange={formik.handleChange}/>
+      <Styled.Error variant="PasswordTileTitle">{formik.errors.title}</Styled.Error>
+      {/*Description*/}
+      <Styled.TileTitle variant="PasswordTileTitle">Opis</Styled.TileTitle>
+      {renderRichText}
+      <RgbButton text="Publish" />
+      </Styled.Form>
     </Styled.Wrapper>
-
   )
 }
-
-export default TuitionRegister
+export default TuitionRegister;
--- tunein-client/src/components/organisms/TuitionRegister/styles.ts
+++ tunein-client/src/components/organisms/TuitionRegister/styles.ts
@@ -1,8 +1,80 @@
-import styled from "styled-components";
+import { Typography } from "@components/styles/typography";
+import styled, { keyframes } from "styled-components";
 
 export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+`;
+
+export const Form = styled.form`
+    min-height: 27rem;
+    background-color: ${({ theme }) => theme.colors.darkMainD};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 2rem;
+    border-radius: 0.2rem;
+`;
+
+export const TileTitle = styled(Typography)`
+    width: 100%;
+    margin-left: 3rem;
+`;
+
+const errorAnim = keyframes`
+    0% { opacity: 1; }
+    10% { opacity: 0.9; }
+    20% { opacity: 0.7; }
+    30% { opacity: 0.5 }
+    40% { opacity: 0.4; }
+    50% { opacity: 0.3; }
+    60% { opacity: 0.3; }
+    70% { opacity: 0.4; }
+    80% { opacity: 0.5; }
+    90% { opacity: 0.9; }
+    100% { opacity: 1; }
+`
+export const Error = styled(Typography)`
+  /* Animation */
+  animation-name: ${errorAnim};
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  /* Color */
+  background-image: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+  background-size: 100%;
+  background-repeat: repeat;
+  background-clip: initial;/*idk, error doesn't show up this way*/
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent; 
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
+  text-shadow: rgba(151, 65, 252, 0.2) 0.063rem 0.063rem 0.063rem;
+`;
+
+export const Input = styled.textarea`
+    box-shadow: inset 0 0 1rem rgb(0 0 0 / 30%);
+    background-color: ${({ theme }) => theme.colors.darkMain};
+    color: white;
+    font-size: 2rem;
+    font-family: ${({theme}) => theme.fonts.body};
+    border: none;
+    border-radius: 0.2rem;
+    padding-left: 0.5rem;
+    resize: none;
+    box-sizing: border-box;
+    display: flex;
+    overflow:hidden;
+    min-height:100%;
+    /* Fix for background color change */
+    &:-internal-autofill-selected,
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active{
+        transition: background-color 600000s 0s, color 600000s 0s;
+    }
 `;
