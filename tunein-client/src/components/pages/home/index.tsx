4d28f6c3b2513a1eb0157dff065f990c799943c8
Milosz <milosz.alejster@gmail.com>
2023-02-23 01:11:02 +0100

    added homecounter

--- tunein-client/src/components/pages/home/index.tsx
+++ tunein-client/src/components/pages/home/index.tsx
@@ -5,13 +5,15 @@ import TuitionRegister from '@components/organisms/TuitionRegister';
 import Heropage from '@components/organisms/Heropage';
 import HomeRegister from '@components/organisms/HomeRegister';
 import HomeBrowse from '@components/organisms/HomeBrowse';
+import HomeCounter from '@components/organisms/HomeCounter';
 //TODO: consider adding scrollIntoView here instead of event in a single section(heropage)
 const Homepage: NextPage = () =>{
     return(
         <>
             <Heropage/>
-            <HomeRegister/>
             <HomeBrowse/>
+            <HomeRegister/>
+            <HomeCounter/>
         </>
     );
 }
