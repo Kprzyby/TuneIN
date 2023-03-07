74d49d96c6c508b40a39676dfae8c84da9aca9ac
Kprzyby <kprzy@vp.pl>
2023-03-07 14:10:07 +0100

    added DTOs for the tutorship

---
+++ Data/DTOs/Tutorship/GetTutorshipsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Data.DTOs.Tutorship
+{
+    public class GetTutorshipsDTO
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public KeyValuePair<string, string>? SortInfo { get; set; }
+        public string? TitleFilterValue { get; set; }
+        public string? CategoryFilterValue { get; set; }
+    }
+}
---
+++ Data/DTOs/Tutorship/UpdateTutorshipDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Data.DTOs.Tutorship
+{
+    public class UpdateTutorshipDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Details { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
