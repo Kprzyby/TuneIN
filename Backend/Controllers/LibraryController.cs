7f5c6acbae39963e0412afbe0ea47a75324a1457
Gabriel Sawicki <sawicki.gabriel@wp.pl>
2023-03-20 12:06:56 +0100

    poprawa api

--- Backend/Controllers/LibraryController.cs
+++ Backend/Controllers/LibraryController.cs
@@ -5,6 +5,7 @@ using Services;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using Data.DTOs.Library;
+using System.Diagnostics;
 
 namespace Backend.Controllers
 {
@@ -26,71 +27,42 @@ namespace Backend.Controllers
 
         #endregion Constructors
 
-        #region Methods
-
+        #region Methods        
         [HttpGet]
         [Route("Library/GetTracks")]
         public async Task<IActionResult> GetTracksAsync()
         {
-            var tracks = _libraryService.GetTracksAsync();
+            var tracks = await _libraryService.GetTracksAsync();
             return Ok(tracks);
         }
         [HttpGet]
         [Route("Library/GetTracksFiltered")]
         public async Task<IActionResult> GetTracksFilteredByTrackNameAsync(string trackName)
         {
-            var tracks = _libraryService.GetTracksFilteredByTrackNameAsync(trackName);
+            var tracks = await _libraryService.GetTracksFilteredByTrackNameAsync(trackName);
             return Ok(tracks);
         }
 
-
-        [HttpGet]
-        [Route("Library/GetTrackInfo")]
-        public async Task<IActionResult> GetTrackInfoAsync(string artist, string trackName)
-        {
-            Console.WriteLine("Works");
-            var trackInfo = await _libraryService.GetTrackInfoAsync(artist, trackName);
-            if (trackInfo == null) return BadRequest("Error");
-            if ((string)trackInfo["message"] == "Track not found") return BadRequest("Not Found");
-            TrackViewModel trackViewModel = new TrackViewModel
-            {
-                TrackName = (string)trackInfo["track"]["name"],
-                Band = (string)trackInfo["track"]["artist"]["name"],
-                Genre = (string)trackInfo["track"]["toptags"]["tag"][0]["name"],
-                LinkToCover = (string)trackInfo["track"]["album"]["image"][3]["#text"]
-            };
-
-            return Ok(trackViewModel);
-        }
-
-        [HttpGet]
-        [Route("Library/GetSearchList")]
-        public async Task<IActionResult> GetSearchListAsync(string name)
-        {
-            Console.WriteLine("Works");
-            var trackList = await _libraryService.GetSearchListAsync(name);
-            if (trackList == null) return BadRequest("Error");
-            return Ok(trackList);
-        }
-
+        
         [HttpPost]
         [Route("Library/AddNewTrack")]
         public async Task<IActionResult> AddTrackAsync(TrackViewModel trackInfo)
         {
-            
+
             bool trackExists = await _libraryService.CheckIfTrackExistsAsync(trackInfo.Band, trackInfo.TrackName);
 
             if (trackExists == true)
             {
                 return StatusCode(409, "This track already exists in library");
             }
-            
+
             TrackInfoDTO trackInfoDTO = new TrackInfoDTO()
             {
-                TrackName = trackInfo.TrackName, 
+                MbId= trackInfo.MbId,
+                TrackName = trackInfo.TrackName,
                 Band = trackInfo.Band,
                 Genre = trackInfo.Genre,
-                LinkToCover= trackInfo.LinkToCover,
+                LinkToCover = trackInfo.LinkToCover,
             };
 
             var createResult = await _libraryService.AddTrackAsync(trackInfoDTO);
---
+++ Services/ApiCallsService.cs
@@ -0,0 +1,76 @@
+﻿using Data.Repositories;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ApiCallsService
+    {
+        #region Properties
+        private const string APIKEY = "";//TO HIDE
+
+        #endregion Properties
+        #region Constructors
+
+
+
+        #endregion Constructors
+        #region Methods
+
+        public async Task<JObject> GetTrackInfoAsync(string artist, string trackName)
+        {
+            //var APIKEY = Environment.GetEnvironmentVariable("APIKEY");
+            var httpClient = new HttpClient();
+            var requestUri = $"http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={APIKEY}&artist={artist}&track={trackName}&format=json";
+            var response = await httpClient.GetAsync(requestUri);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(responseContent);
+        }
+        /*
+        public async Task<JObject> GetSearchListAsync(string track)
+        {
+
+            var httpClient = new HttpClient();
+            var requestUri = $"http://ws.audioscrobbler.com/2.0/?method=track.search&track={track}&api_key={APIKEY}&format=json";
+            var response = await httpClient.GetAsync(requestUri);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(responseContent);
+        }
+        */
+        public async Task<string> GetSearchListAsync(string trackName)
+        {
+            //var APIKEY = Environment.GetEnvironmentVariable("APIKEY");
+            var url = "http://ws.audioscrobbler.com/2.0/?method=track.search" +
+                "&api_key=" + APIKEY + "&track=" + trackName + "&limit=10" +
+                "&format=json";
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                JArray trackArray = JObject.Parse(jsonResponse)["results"]["trackmatches"]["track"].ToObject<JArray>();
+
+                List<JObject> trackList = new List<JObject>();
+                foreach (var trackObject in trackArray)
+                {
+                    trackList.Add(new JObject(
+                        new JProperty("name", trackObject["name"]),
+                        new JProperty("artist", trackObject["artist"]),
+                        new JProperty("url", trackObject["url"])
+                    ));
+                }
+
+                return JsonConvert.SerializeObject(trackList);
+            }
+        }
+
+        #endregion Methods
+    }
+}
