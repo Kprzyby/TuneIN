087c7da75bac5f7babd8367aea592ab4d9c91726
Konrad <konradwasilewski@vp.pl>
2023-03-29 14:57:32 +0200

    hotfix

--- tunein-client/src/components/organisms/Announcements/index.tsx
+++ tunein-client/src/components/organisms/Announcements/index.tsx
@@ -10,6 +10,13 @@ const Announcements: React.FC = () => {
 
     return (
         <Styled.AnnouncementsPage>
+            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
         </Styled.AnnouncementsPage>
     )
 }
--- tunein-client/src/components/pages/announcements/index.tsx
+++ tunein-client/src/components/pages/announcements/index.tsx
@@ -4,7 +4,7 @@ import { NextPage } from 'next'
 
 const Homepage: NextPage = () =>{
     return(
-        <Announcement title='SumTitle'/>
+        <Announcements/>
     );
 }
 
