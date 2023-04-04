1694133c1081f191ecd31afa2610cf037d6b48f4
Konrad <konradwasilewski@vp.pl>
2023-04-04 20:44:01 +0200

    betterImages

--- tunein-client/src/components/organisms/Announcements/index.tsx
+++ tunein-client/src/components/organisms/Announcements/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react"
 import Announcement from '@components/organisms/Announcement';
-import * as Endpoint from '../../../api/endpoint'
+import {createDBEndpoint, ENDPOINTS} from '../../../api/endpoint'
 import * as Styled from "./styles"
 
 const Announcements: React.FC = () => {
@@ -8,15 +8,17 @@ const Announcements: React.FC = () => {
     // announcementsList : [] = [];
     // Endpoint.createDBEndpoint(Endpoint.BASE_URL + Endpoint.ENDPOINTS.)
 
+    createDBEndpoint(ENDPOINTS.tutorship.getusertutorships)
+
     return (
         <Styled.AnnouncementsPage>
-            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
-            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
-            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
-            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
-            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
-            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
-            <Announcement title="Title" interested={100} img="https://images.unsplash.com/photo-1593642532979-7c1a43b0b6ac?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"/>
+            <Announcement title="Title" interested={100} img="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"/>
+            <Announcement title="Title" interested={100} img="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"/>
+            <Announcement title="Title" interested={100} img="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"/>
+            <Announcement title="Title" interested={100} img="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"/>
+            <Announcement title="Title" interested={100} img="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"/>
+            <Announcement title="Title" interested={100} img="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"/>
+            <Announcement title="Title" interested={100} img="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"/>
         </Styled.AnnouncementsPage>
     )
 }
