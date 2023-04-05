4f1e2e660829d19ef4c676e3e3815a8906d29dfb
Kprzyby <kprzy@vp.pl>
2023-04-05 23:34:12 +0200

    created a unit test for getting the categories of the tutorship

---
+++ UnitTests/TutorshipTests/GetCategoriesTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTests.TutorshipTests
+{
+    public class GetCategoriesTests : BaseTutorshipTests
+    {
+        [Fact]
+        public void GetCategories_WhenAllIsGood_ShouldReturnCategories()
+        {
+            //arrange
+
+            //act
+            var result = tutorshipService.GetCategories();
+
+            //assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal<int>(200, result.StatusCode);
+            Assert.NotNull(result.Result);
+            Assert.NotEmpty((List<string>)result.Result);
+        }
+    }
+}
