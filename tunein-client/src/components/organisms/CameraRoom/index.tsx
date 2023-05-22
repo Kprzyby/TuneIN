eeb7db8ad389fc968f6cf232e5d8d87e4d776ef3
Michał Ryniewicz <misiekxyz18@gmail.com>
2023-05-22 15:34:29 +0200

    refactor

--- tunein-client/src/components/organisms/CameraRoom/index.tsx
+++ tunein-client/src/components/organisms/CameraRoom/index.tsx
@@ -1,22 +1,132 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import LocalCamera from "../LocalCamera"
 import RemoteCamera from "../RemoteCamera"
+import { Socket } from "socket.io-client"
 import * as Styled from "./styles"
+import Peer from "peerjs"
 
-const CameraRoom: React.FC = () => {
-    
+interface CameraRoomProps {
+    socket: Socket | undefined;
+    room: string;
+}
+
+const CameraRoom: React.FC<CameraRoomProps> = ({ socket, room }) => {
+
     //var testStream: MediaStream = new MediaStream();
+
+    const [peerId, setPeerId] = useState("")
+    const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+    const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
+    const [peer, setPeer] = useState<Peer | null>(null);
+
+    var disconnect: Function
+
+    useEffect(() => {
+
+        const fn = async () => {
+            const PeerJs = (await import('peerjs')).default;
+            // set it to state here
+            setPeer(new PeerJs())
+        }
+
+        fn()
+
+        // peer.on('open', id => {
+        //     setPeerId(id)
+        // })
+
+    }, [])
+
+    useEffect(() => {
+
+        //import("peerjs").then(({ default: Peer }) => {
+        //const peer = new Peer()
+
+        peer?.on('open', id => {
+            console.log('My id is: ' + id)
+            setPeerId(id)
+        })
+
+        //const videoGrid = document.getElementById("videoGrid")
+
+        //const myVideo = document.createElement("video")
+        //myVideo.muted = true
+
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        }).then(stream => {
+            setLocalStream(stream)
+            //addVideoStream(myVideo, stream)
+
+            peer?.on("call", call => {
+                console.log(`User answered`)
+                call.answer(stream)
+                //const video = document.createElement("video")
+                call.on("stream", userVideoStream => {
+                    //addVideoStream(video, userVideoStream)
+                    setRemoteStream(userVideoStream)
+                })
+            })
+
+            if (socket) {
+                socket.on("user-connected", userId => {
+                    console.log("User connected: " + userId)
+                    console.log(peer);
+                    if (userId !== peer?.id) {
+                        console.log("USER trying to connect")
+                        connectToNewUser(userId, stream)
+                    }
+                })
+
+                socket.emit("join", room, peer?.id)
+            }
+        })
+
+        function connectToNewUser(userId: string, stream: MediaStream) {
+            const call = peer?.call(userId, stream)
+            //const video = document.createElement("video")
+
+            console.log(`User called`)
+
+            call?.on("stream", userVideoStream => {
+                //addVideoStream(video, userVideoStream)
+                setRemoteStream(userVideoStream)
+            })
+
+            call?.on("close", () => {
+                //video.remove()
+            })
+        }
+
+        /*function addVideoStream(video: HTMLVideoElement, stream: MediaStream) {
+            video.srcObject = stream
+            video.addEventListener('loadedmetadata', () => {
+                video.play()
+            })
+            if (videoGrid) {
+                videoGrid.append(video)
+            }
+        }*/
+
+        //disconnect = peer?.disconnect
+        //})
+
+        return () => {
+            peer?.disconnect
+        }
+    }, [peer])
 
     return (
         <Styled.CameraRoom>
-            <LocalCamera />
+            <LocalCamera localStream={localStream} />
             <Styled.RemoteCameras>
-                <Styled.TestItem/>
-                <Styled.TestItem/>
-                <Styled.TestItem/>
-                <Styled.TestItem/>
+                <Styled.TestItem />
+                <Styled.TestItem />
+                <Styled.TestItem />
+                <Styled.TestItem />
+                <RemoteCamera remoteStream={remoteStream} />
             </Styled.RemoteCameras>
-            {/* <RemoteCamera remoteStream={} /> */}
         </Styled.CameraRoom>
     )
 }
--- tunein-client/src/components/organisms/LocalCamera/index.tsx
+++ tunein-client/src/components/organisms/LocalCamera/index.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useRef, useState } from "react"
 import * as Styled from "./styles"
 import ToggleButton from "../../molecules/ToggleButton"
 
-const LocalCamera: React.FC = () => {
+interface LocalCameraProps {
+    localStream: MediaStream | null;
+}
+
+const LocalCamera: React.FC<LocalCameraProps> = ({localStream}) => {
 
     const videoRef = useRef<HTMLVideoElement | null>(null);
     const audioRef = useRef<HTMLAudioElement | null>(null);
@@ -13,19 +17,23 @@ const LocalCamera: React.FC = () => {
     const [stream, setStream] = useState<MediaStream | null>(null);
 
     useEffect(() => {
-        navigator.mediaDevices.getUserMedia({ video: isAudioEnabled, audio: isAudioEnabled })
-            .then(stream => {
-                setStream(stream);
-                if (videoRef.current && stream && isVideoEnabled) {
-                    videoRef.current.srcObject = stream;
+        /*navigator.mediaDevices.getUserMedia({ video: isAudioEnabled, audio: isAudioEnabled })
+            .then(stream => {*/
+                setStream(localStream);
+                if (videoRef.current && localStream && isVideoEnabled) {
+                    videoRef.current.srcObject = localStream;
                 }
-                if (audioRef.current && stream && isAudioEnabled) {
-                    audioRef.current.srcObject = stream;
+                if (audioRef.current && localStream && isAudioEnabled) {
+                    audioRef.current.srcObject = localStream;
                 }
-            })
+
+                /*video.addEventListener('loadedmetadata', () => {
+                    video.play()
+                })*/
+            /*})
             .catch(error => {
                 console.log('Error getting user media: ', error);
-            });
+            });*/
             return () => {
                 if (stream) {
                     stream.getTracks().forEach(track => {
@@ -33,11 +41,11 @@ const LocalCamera: React.FC = () => {
                     });
                 }
             }
-    }, [])
+    }, [localStream])
 
     const handleAudioToggle = () => {
-        if (stream) {
-            const audioTrack = stream.getAudioTracks()[0];
+        if (localStream) {
+            const audioTrack = localStream.getAudioTracks()[0];
             setIsAudioEnabled(!isAudioEnabled);
             if (audioTrack) {
                 audioTrack.enabled = !audioTrack.enabled;
@@ -46,8 +54,8 @@ const LocalCamera: React.FC = () => {
     }
 
     const handleVideoToggle = () => {
-        if (stream) {
-            const videoTrack = stream.getVideoTracks()[0];
+        if (localStream) {
+            const videoTrack = localStream.getVideoTracks()[0];
             setIsVideoEnabled(!isVideoEnabled);
             if (videoTrack) {
                 videoTrack.enabled = !videoTrack.enabled;
@@ -58,7 +66,7 @@ const LocalCamera: React.FC = () => {
     return (
         <Styled.CameraWithButtons>
             <Styled.CameraWrapper>
-                <Styled.LocalCamera ref={videoRef} style={{display: isVideoEnabled ? 'block' : 'none'}} autoPlay muted />
+                <Styled.LocalCamera ref={videoRef} style={{display: isVideoEnabled ? 'block' : 'none'}} autoPlay={true} muted={true} />
                 <audio ref={audioRef} autoPlay />
             </Styled.CameraWrapper>
             <Styled.CameraButtons>
