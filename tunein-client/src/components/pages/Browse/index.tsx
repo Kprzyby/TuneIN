716f80145598e621ce02606200c7739aa147fe68
Konrad Wasilewski <konradwasilewski@vp.pl>
2023-05-24 12:48:34 +0200

    Little fix

--- tunein-client/src/components/pages/Browse/index.tsx
+++ tunein-client/src/components/pages/Browse/index.tsx
@@ -6,9 +6,9 @@ import ObjectListing from '@components/organisms/ObjectListing';
 import * as Styled from './styles';
 
 const Browser: NextPage = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState<Array<any>>([])
   const [option, setOption] = useState('')
-  const [sortedData, setSortedData] = useState([])
+  const [sortedData, setSortedData] = useState<Array<any>>([])
 
   useEffect(() => {
     let endpoint = ''
@@ -29,7 +29,21 @@ const Browser: NextPage = () => {
     setOption(option)
   }
 
+  const SampleData = [
+    { id: 1, name: 'Product A', price: 10.99, category: 'Electronics' },
+    { id: 2, name: 'Product B', price: 19.99, category: 'Clothing' },
+    { id: 3, name: 'Product C', price: 7.99, category: 'Home & Kitchen' },
+    { id: 4, name: 'Product D', price: 5.49, category: 'Electronics' },
+    { id: 5, name: 'Product E', price: 14.99, category: 'Clothing' },
+    { id: 6, name: 'Product F', price: 9.99, category: 'Home & Kitchen' },
+    { id: 7, name: 'Product G', price: 8.49, category: 'Electronics' },
+    { id: 8, name: 'Product H', price: 24.99, category: 'Clothing' },
+    { id: 9, name: 'Product I', price: 12.99, category: 'Home & Kitchen' },
+    { id: 10, name: 'Product J', price: 6.99, category: 'Electronics' }
+  ];
+
   const handleSorying = (option: string) => {
+    setData(SampleData)
     const sortedData = data.sort((a, b) => {
       if (a[option] < b[option]) {
         return -1;
@@ -44,27 +58,12 @@ const Browser: NextPage = () => {
     setSortedData(sortedData);
   }
 
-  const SampleData = [
-    { id: 1, name: 'Product A', price: 10.99, category: 'Electronics' },
-    { id: 2, name: 'Product B', price: 19.99, category: 'Clothing' },
-    { id: 3, name: 'Product C', price: 7.99, category: 'Home & Kitchen' },
-    { id: 4, name: 'Product D', price: 5.49, category: 'Electronics' },
-    { id: 5, name: 'Product E', price: 14.99, category: 'Clothing' },
-    { id: 6, name: 'Product F', price: 9.99, category: 'Home & Kitchen' },
-    { id: 7, name: 'Product G', price: 8.49, category: 'Electronics' },
-    { id: 8, name: 'Product H', price: 24.99, category: 'Clothing' },
-    { id: 9, name: 'Product I', price: 12.99, category: 'Home & Kitchen' },
-    { id: 10, name: 'Product J', price: 6.99, category: 'Electronics' }
-  ];
-
-  setData(SampleData)
-
   return (
   <Styled.Wrapper>
     <Styled.OptionsWrapper>
       <a onClick={() => handleOptionChange('tuitions')}>Tuitions</a>
       <a onClick={() => handleOptionChange('playlists')}>Playlists</a>
-      <a onClick={() => handleSorying('name')}>Name</a>
+      <a onClick={() => handleSorying('price')}>Name</a>
     </Styled.OptionsWrapper>
 
     <ObjectListing objects={sortedData} />
