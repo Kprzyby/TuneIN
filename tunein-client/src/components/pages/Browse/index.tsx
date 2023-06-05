001f338a898257270c41f487d04aa2a4e7103383
Milosz <milosz.alejster@gmail.com>
2023-06-05 19:51:24 +0200

    Versioning untracked files

--- tunein-client/src/components/pages/Browse/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-import React from "react";
-import Announcements from "@components/organisms/Announcements";
-
-const Browser: NextPage = () => <Announcements />;
-
-export default Browser;
