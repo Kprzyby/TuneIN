f0c1746b90e0e52e1efbd236cc2730dcdd5a078c
Milosz <milosz.alejster@gmail.com>
2023-06-05 22:40:05 +0200

    added env variables to backend url

--- tunein-client/src/api/endpoint/index.ts
+++ tunein-client/src/api/endpoint/index.ts
@@ -1,7 +1,7 @@
 // const axios = require("axios");
 import axios from "axios";
 
-export const BASE_URL = "https://localhost:7074/";
+export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const ENDPOINTS = {
   auth: {
